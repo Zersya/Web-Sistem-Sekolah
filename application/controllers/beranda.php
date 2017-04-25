e9650b6df99a5bdc981e67f1751dc42dac852814
salmafauziah <salmafauziah481@gmail.com>
2017-04-25 15:19:44 +0700

    Tampilan Awal Murid
    Tampilan Awal Murid / sesudah murid login langsung ke beranda

--- application/controllers/beranda.php
+++ application/controllers/beranda.php
@@ -8,13 +8,27 @@
     function __construct()
     {
       parent::__construct();
+	  $this->load->model('m_profil'); 
+	  $this->load->model('m_admin'); 
     }
 
     function index(){
+	  $this->load->view('header_murid', $this->getDataMurid()); 
       $this->load->view('v_muridutama_beranda');
     }
-
-    
+	function getDataMurid(){
+      $where = $this->session->nis;
+      $query = $this->m_admin->scan_profil("select Nis, Nama_murid, foto from murid where nis = '".$where."'");
+      foreach ($query->result() as $row) {
+        $dataMurid = array(
+          'nis'   => $row->Nis,
+          'nama_murid'  => $row->Nama_murid,
+          'foto'  => $row->foto
+        );
+        $data = array('data' => $dataMurid);
+      }
+	  
+      return $data;
+    } 
   }
-
  ?>
